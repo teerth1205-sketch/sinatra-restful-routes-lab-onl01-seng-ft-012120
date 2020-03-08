91316c33b20c0f5384dd0cf393ed2f8766bac7b0
Teerth Patel <teerth1205@gmail.com>
2020-03-08 22:47:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,7 +50,7 @@ class ApplicationController < Sinatra::Base
     delete '/recipes/:id' do 
       @recipe = Recipe.find(params[:id])
       @recipe.destroy
-      
+      redirect '/recipes'
     end 
 end 
   
